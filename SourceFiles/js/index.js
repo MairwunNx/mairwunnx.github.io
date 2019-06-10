243c67ec24cc497913c839ef900aa7a2b2789299
Pavel Erokhin <MairwunNx@gmail.com>
2019-06-10 19:11:45 +0300

    test commit

--- SourceFiles/js/index.js
+++ SourceFiles/js/index.js
@@ -1,20 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
+    alert("Loaded")
     let bottomArrow = document.getElementById('bottom-button')
     bottomArrow.addEventListener('click', function () {
         scrollPage('description')
     })
 })
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+let vh = window.innerHeight * 0.01
+document.documentElement.style.setProperty('--vh', `${vh}px`)
 
 window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
 });
 
 function scrollPage(div) {
-    let target = document.getElementById(div);
+    let target = document.getElementById(div)
     target.scrollIntoView({
         behavior: "smooth",
         block: "end",
