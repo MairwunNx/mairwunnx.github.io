03d9b6835ba50f402cbef5caa5e7d88301ee5276
Pavel Erokhin <MairwunNx@gmail.com>
2019-06-10 18:45:01 +0300

    Fixed wrong layout while resize or change size.

---
+++ SourceFiles/js/index.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let bottomArrow = document.getElementById('bottom-button')
+    bottomArrow.addEventListener('click', function () {
+        scrollPage('description')
+    })
+})
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+function scrollPage(div) {
+    let target = document.getElementById(div);
+    target.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest"
+    })
+}
--- index.js
+++
@@ -1,6 +0,0 @@
-function scrollPage() {
-    var height = window.innerHeight
-        || document.documentElement.clientHeight
-        || document.body.clientHeight;
-    scroll(0, height, 'smooth');
-}
