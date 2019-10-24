5156c89ff2864d22aa9f5b1d376bb123367eed3d
Pavel Erokhin <MairwunNx@gmail.com>
2019-10-24 08:27:28 +0300

    Added Projects section. Improved code.

--- SourceFiles/js/index.js
+++ SourceFiles/js/index.js
@@ -2,9 +2,7 @@ if (document.readyState !== 'loading') {
     let vh = window.innerHeight * 0.01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
     let bottomArrow = document.getElementById('bottom-button')
-    bottomArrow.addEventListener('click', function () {
-        scrollPage('mairwunnx-summary')
-    })
+    subscribeEvents(bottomArrow)
     processVisibilityUlSwitchers()
     addResizeListener()
 } else {
@@ -12,12 +10,16 @@ if (document.readyState !== 'loading') {
     document.documentElement.style.setProperty('--vh', `${vh}px`)
     let bottomArrow = document.getElementById('bottom-button')
     document.addEventListener('DOMContentLoaded', function () {
-        bottomArrow.addEventListener('click', function () {
-            scrollPage('mairwunnx-summary')
-        })
+        subscribeEvents(bottomArrow)
     });
     processVisibilityUlSwitchers()
     addResizeListener()
+}
+
+function subscribeEvents(element) {
+    element.addEventListener('click', function () {
+        scrollPage('mairwunnx-summary')
+    })
 }
 
 function addResizeListener() {
@@ -43,6 +45,9 @@ function processVisibilityUlSwitchers() {
     let skillsCheckBox = document.getElementById('skills-checkbox')
     let skillsList = document.getElementById('skills-list')
 
+    let projectsCheckBox = document.getElementById('projects-checkbox')
+    let projectsList = document.getElementById('projects-list')
+
     let funFactsCheckBox = document.getElementById('fun-facts-checkbox')
     let funFactsList = document.getElementById('fun-facts-list')
 
@@ -51,51 +56,27 @@ function processVisibilityUlSwitchers() {
 
     aboutCheckBox.checked = true
     skillsCheckBox.checked = false
+    projectsCheckBox.checked = false
     funFactsCheckBox.checked = false
     contactCheckBox.checked = false
+    
+    subscribeEvents(aboutCheckBox, aboutList, "About MairwunNx")
+    subscribeEvents(skillsCheckBox, skillsList, "MairwunNx's Skills")
+    subscribeEvents(projectsCheckBox, projectsList, "MairwunNx's Projects")
+    subscribeEvents(funFactsCheckBox, funFactsList, "MairwunNx's Fun Facts")
+    subscribeEvents(contactCheckBox, contactList, "Contact with Me")
 
-    aboutCheckBox.addEventListener('click', function () {
-        if (aboutCheckBox.checked) {
-            aboutCheckBox.checked = false
-            aboutCheckBox.innerHTML = "\\ About MairwunNx"
-            aboutList.style = "display: none;"
-        } else {
-            aboutCheckBox.checked = true
-            aboutCheckBox.innerHTML = "/ About MairwunNx"
-            aboutList.style = "display: block;"
-        }
-    })
-    skillsCheckBox.addEventListener('click', function () {
-        if (skillsCheckBox.checked) {
-            skillsCheckBox.checked = false
-            skillsCheckBox.innerHTML = "\\ MairwunNx's Skills"
-            skillsList.style = "display: none;"
-        } else {
-            skillsCheckBox.checked = true
-            skillsCheckBox.innerHTML = "/ MairwunNx's Skills"
-            skillsList.style = "display: block;"
-        }
-    })
-    funFactsCheckBox.addEventListener('click', function () {
-        if (funFactsCheckBox.checked) {
-            funFactsCheckBox.checked = false
-            funFactsCheckBox.innerHTML = "\\ MairwunNx's Fun Facts"
-            funFactsList.style = "display: none;"
-        } else {
-            funFactsCheckBox.checked = true
-            funFactsCheckBox.innerHTML = "/ MairwunNx's Fun Facts"
-            funFactsList.style = "display: block;"
-        }
-    })
-    contactCheckBox.addEventListener('click', function () {
-        if (contactCheckBox.checked) {
-            contactCheckBox.checked = false
-            contactCheckBox.innerHTML = "\\ Contact with Me"
-            contactList.style = "display: none;"
-        } else {
-            contactCheckBox.checked = true
-            contactCheckBox.innerHTML = "/ Contact with Me"
-            contactList.style = "display: block;"
-        }
-    })
+    function subscribeEvents(checkbox, list, caption) {
+        checkbox.addEventListener('click', function() {
+            if (checkbox.checked) {
+                checkbox.checked = false
+                checkbox.innerHTML = "\\ " + caption
+                list.style = "display: none;"
+            } else {
+                checkbox.checked = true
+                checkbox.innerHTML = "/ " + caption
+                list.style = "display: block;"
+            }
+        })
+    }
 }
